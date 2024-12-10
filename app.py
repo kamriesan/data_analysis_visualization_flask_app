c64793e65fa1eb2d2bdc7d35d636724abda14c40
kamriesan <gangoso.mikaella@gmail.com>
2024-12-11 04:57:47 +0800

    updated app.py ui

--- app.py
+++ app.py
@@ -45,37 +45,6 @@ st.markdown(
 )
 
 
-# # Example 1: Style a button
-# with stylable_container(
-#     key="green_button",
-#     css_styles="""
-#         button {
-#             background-color: green;
-#             color: white;
-#             border-radius: 20px;
-#         }
-#     """
-# ):
-#     st.button("Green Button")  # Styled button
-
-# st.button("Normal Button")  # Default button
-
-# # Example 2: Style a container
-# with stylable_container(
-#     key="container_with_border",
-#     css_styles="""
-#         {
-#             border: 1px solid rgba(49, 51, 63, 0.2);
-#             border-radius: 0.5rem;
-#             padding: calc(1em - 1px);
-#             background-color: white;
-#         }
-#     """
-    
-# ):
-#     st.markdown("This is a container with a border.")  # Styled container
-
-
 
 # Function to encode an image as Base64 for fast loading
 def get_base64_image(image_path):
@@ -188,24 +157,18 @@ if "generated_charts" not in st.session_state:
 if "chart_displayed" not in st.session_state:
     st.session_state.chart_displayed = False  # Ensure only one chart is displayed
 
-# Header with centered title
+# STYLES
 st.markdown(
     """
     <div style='text-align: center;'>
-        <h1 style='font-family: "Poppins", sans-serif; color: white;'>
+        <h1 style='font-family: "Poppins", sans-serif; color: white; margin-top: -30px;'>
             🔮 DATAGENIE AI
         </h1>
         <h5 style='font-family: "Poppins", sans-serif; color: #BBBBBB; margin-top: -10px;'>
             Transform Your Data into Powerful Visualizations & AI Insights!
         </h5>
     </div>
-    """,
-    unsafe_allow_html=True,
-)
 
-
-st.markdown(
-    """
     <style>
     /* Target the file uploader */
     div[data-testid="stFileUploader"] {
@@ -217,51 +180,8 @@ st.markdown(
         width: 85%;
         margin: auto; /* Center align */
     }
-    div[data-testid="stFileUploader"] > label {
-        color: white; /* Text color for label */
-        font-size: 16px; /* Font size for label */
-    }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
 
-
-
-
-# Main content logic
-
-uploaded_file = st.file_uploader("Upload your CSV file", type="csv")
-
-st.markdown(
-    """
-    <style>
-    /* Custom styling for the two-column container */
-    .custom-columns {
-        display: flex; /* Flexbox layout for columns */
-        justify-content: space-between; /* Space between columns */
-        gap: 20px; /* Spacing between columns */
-        background-color: white; /* Dark grey background */
-        border: 2px solid #4CAF50; /* Green border */
-        border-radius: 10px; /* Rounded corners */
-        padding: 40px; /* Internal padding */
-        width: 50px; /* Max width of the container */
-        margin: auto; /* Center align the container */
-        color: white; /* Text color */
-    }
-    .custom-column {
-        flex: 1; /* Equal width for columns */
-        padding: 10px; /* Internal padding for columns */
-    }
-    </style>
-
-    """,
-    unsafe_allow_html=True,
-)
-
-st.markdown(
-    """
-    <style>
+    /* Target the horizontal block */
     div[data-testid="stHorizontalBlock"] {
         display: flex;
         justify-content: space-between;
@@ -272,16 +192,39 @@ st.markdown(
         color: white;
         width: 85%;
         margin: auto;
-
     }
-    div[data-testid="column"] {
-        flex: 1;
-        padding: 10px;
+    div[data-testid="stSelectbox"] > div > input {
+        font-size: 16px;
+        padding: 8px;
+        border: 1px solid #cccccc;
+        border-radius: 5px;
+    }
+    div.stButton > button {
+        background-color: #FF5152; /* Violet background */
+        color: white; /* White text */
+        border-radius: 10px; /* Rounded corners */
+        padding: 10px 20px; /* Padding */
+        font-size: 16px; /* Font size */
+        font-weight: bold; /* Bold text */
+        border: none; /* Remove border */
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); /* Add shadow */
+        cursor: pointer; /* Pointer cursor on hover */
+    }
+    div.stButton > button:hover {
+        background-color: #E9292A; /* Darker violet on hover */
+        color: white;
     }
     </style>
     """,
     unsafe_allow_html=True,
 )
+
+
+
+# Main content logic
+
+uploaded_file = st.file_uploader("Is your data ready for enchantment? 🧚‍♀️ Upload your CSV now! 📩", type="csv")
+
 
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
@@ -297,23 +240,26 @@ if uploaded_file is not None:
         st.markdown(
             """
             <h3 style='color: #333333;  font-size: 24px; font-weight: bold;'>
-                👀 Data Preview
+                👀 Data Previews
             </h3>
-            <h6 style='font-family: "Poppins", sans-serif; color: #BBBBBB; margin-top: -10px;'>
-            This is your raw data. What do we do with it?
+            <h6 style='font-family: "Poppins", sans-serif; font-size: 12px; color: #333333; margin-top: -10px;'>
+            Hover over the table to explore, search, or view it in full screen!
             </h6>
             """,
             unsafe_allow_html=True,
         )
-        st.dataframe(df, width=900)  # Adjust width if needed
+        st.dataframe(df, width=900)  
 
     # Add a title and the cleaning report in the right column
     with col2:
         st.markdown(
             """
-            <h3 style='color: #333333; font-size: 24px; font-weight: bold;'>
+            <h3 style='color: #333333;  font-size: 24px; font-weight: bold;'>
                 🧹 Let's Clean Your Data
             </h3>
+            <h6 style='font-family: "Poppins", sans-serif; font-size: 12px; color: #333333; margin-top: -10px;'>
+            Abracadabra, your data is now clean!
+            </h6>
             """,
             unsafe_allow_html=True,
         )
@@ -348,10 +294,7 @@ if uploaded_file is not None:
                 """,
                 unsafe_allow_html=True,
             )
-            st.markdown(
-                "<p class='report-title'>Here:</p>",
-                unsafe_allow_html=True,
-            )
+
             for report in cleaning_report:
                 st.markdown(
                     f"<p class='report'>{report}</p>",
@@ -359,55 +302,86 @@ if uploaded_file is not None:
                 )
         else:
             st.markdown(
-                "<p class='no-cleaning'>No cleaning necessary; data was already clean ✨.</p>",
+                """
+                <style>
+                .no-cleaning { color: #333333; font-size: 16px; font-family: 'Poppins', sans-serif; }
+                </style>
+                <p class='no-cleaning'>No cleaning necessary; data was already clean ✨.</p>
+                """,
                 unsafe_allow_html=True,
             )
 
 
-    column_options = df.columns.tolist()
 
-    x_axis = st.selectbox("X-axis column:", options=column_options)
-    y_axis = st.selectbox("Y-axis column:", options=column_options)
-    chart_type = st.selectbox(
-        "Chart type:", 
-        ["Bar Chart", "Line Chart", "Scatter Plot", "Pie Chart", "Tree Map"]
-    )
 
-    if st.button("Generate Visualization"):
-        chart = None
-        if chart_type == "Bar Chart":
-            chart = px.bar(df, x=x_axis, y=y_axis)
-        elif chart_type == "Line Chart":
-            chart = px.line(df, x=x_axis, y=y_axis)
-        elif chart_type == "Scatter Plot":
-            chart = px.scatter(df, x=x_axis, y=y_axis)
-        elif chart_type == "Pie Chart":
-            chart = px.pie(df, names=x_axis, values=y_axis)
-        elif chart_type == "Tree Map":
-            chart = px.treemap(df, path=[x_axis], values=y_axis)
+    col1, col2 = st.columns([1, 3])
 
-        if chart:
-            st.session_state.generated_charts = [{"chart": chart}]  # Update with only the latest chart
-            st.session_state.chart_displayed = True  # Mark the chart as displayed
+    # Content for col1
+    with col1:
+        st.markdown(
+            """
+            <h3 style='color: #333333; font-size: 20px; font-weight: bold;'>
+                🧙‍♂️ Chart Wizardry
+            </h3>
+            <h6 style='font-family: "Poppins", sans-serif; font-size: 12px; color: #333333; margin-top: -10px;'>
+                Bring your chart to life!
+            </h6>
+            """,
+            unsafe_allow_html=True,
+        )
 
-# Display the latest chart only when not already displayed
-if st.session_state.chart_displayed:
-    last_chart = st.session_state.generated_charts[-1]  # Get the last chart
-    st.plotly_chart(last_chart["chart"], key="unique_chart")  # Display the chart once
+        column_options = df.columns.tolist()
 
-    # Visualizer AI Section
-    st.subheader("Visualizer AI")
-    if st.button("Analyze Chart with AI"):
-        st.session_state.chart_displayed = False  # Prevent multiple analyses for the same chart
-        with st.spinner("Analyzing the chart... Please wait."):
-            # Convert the chart to an image for analysis
-            chart_image = fig_to_pil(last_chart["chart"])
-            # Generate AI insights
-            response = get_gemini_response("Analyze this chart", chart_image)
+        # Add your interactive widgets
+        x_axis = st.selectbox("Choose your X column", options=column_options)
+        y_axis = st.selectbox("Choose your Y column", options=column_options)
+        chart_type = st.selectbox(
+            "Any chart type in mind? 🤔",
+            ["Bar Chart", "Line Chart", "Scatter Plot", "Pie Chart", "Tree Map"],
+        )
 
-            # Display the AI-generated insights
-            st.subheader("AI Insights:")
-            if response:
-                st.write(response)
-            else:
-                st.write("No insights generated. Please try again.")
+        # Move the button to the bottom of col1
+        generate_chart = st.button("Generate Visualization")
+
+    # Content for col2
+    if generate_chart:  # Check if the button is pressed
+        with col2:
+            chart = None
+            if chart_type == "Bar Chart":
+                chart = px.bar(df, x=x_axis, y=y_axis)
+            elif chart_type == "Line Chart":
+                chart = px.line(df, x=x_axis, y=y_axis)
+            elif chart_type == "Scatter Plot":
+                chart = px.scatter(df, x=x_axis, y=y_axis)
+            elif chart_type == "Pie Chart":
+                chart = px.pie(df, names=x_axis, values=y_axis)
+            elif chart_type == "Tree Map":
+                chart = px.treemap(df, path=[x_axis], values=y_axis)
+
+            if chart:
+                st.session_state.generated_charts = [{"chart": chart}]  # Update with only the latest chart
+                st.session_state.chart_displayed = True  # Mark the chart as displayed
+
+    # Display the latest chart only when not already displayed
+    if st.session_state.chart_displayed:
+        last_chart = st.session_state.generated_charts[-1]  # Get the last chart
+        st.plotly_chart(last_chart["chart"], key="unique_chart")  # Display the chart once
+
+        # Visualizer AI Section
+        st.subheader("Visualizer AI")
+        if st.button("Analyze Chart with AI"):
+            st.session_state.chart_displayed = False  # Prevent multiple analyses for the same chart
+            with st.spinner("Analyzing the chart... Please wait."):
+                # Convert the chart to an image for analysis
+                chart_image = fig_to_pil(last_chart["chart"])
+                # Generate AI insights
+                response = get_gemini_response("Analyze this chart", chart_image)
+
+                # Display the AI-generated insights
+                st.subheader("AI Insights:")
+                if response:
+                    st.write(response)
+                else:
+                    st.write("No insights generated. Please try again.")
+
+                
